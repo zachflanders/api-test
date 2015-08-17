b167bf0fc3a6607de327aa3dbc264250dd8a8bcd
Zach Flanders <zachflanders@gmail.com>
2015-08-17 14:00:15 -0500

    restructing

--- api-test.js
+++ api-test.js
@@ -14,33 +14,33 @@ if (Meteor.isClient) {
 	    var bbox = turf.extent(data);
 	    console.log(bbox);
 	    query = "?$where= loction_1.latitude between'" +bbox[1] +"' and '" + bbox[4] +"'";
+      HTTP.get("https://data.kcmo.org/resource/kbzx-7ehe.json" + query, function(err, result){
+    	   console.log(result);
+    	   L.Icon.Default.imagePath = '/packages/fuatsengul_leaflet/images';
+         var map = L.map('map',{zoomControl:false}).setView([39.0997, -94.5783], 13);
+    	   L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    	      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    	   }).addTo(map);
+    	   ecodistrict.addTo(map);
+    	   result.data.forEach(function(entry, index){
+    	      latlon = [entry.location_1.latitude, entry.location_1.longitude];
+    		    L.marker(latlon).addTo(map).bindPopup(entry.description+', '+entry.from_date);
+    	   });
+    	   return result;
+      });
 	}
 	});
-	  
-   
-   
-    HTTP.get("https://data.kcmo.org/resource/kbzx-7ehe.json" + query, function(err, result){
-	console.log(result);
-	L.Icon.Default.imagePath = '/packages/fuatsengul_leaflet/images';
-	var map = L.map('map',{zoomControl:false}).setView([39.0997, -94.5783], 13);
-	L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-	}).addTo(map);
-	ecodistrict.addTo(map);
-	result.data.forEach(function(entry, index){
-	    
-   		latlon = [entry.location_1.latitude, entry.location_1.longitude];
-		L.marker(latlon).addTo(map).bindPopup(entry.description+', '+entry.from_date);
-	    
-	});
-	return result;});
-
-    
 
 
 
 
-}  
+
+
+
+
+
+
+}
 
 if (Meteor.isServer) {
 	console.log('hello from server');
