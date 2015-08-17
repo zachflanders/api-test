953fa6618de19b1ac2fc988748ba3898687bea82
Ubuntu <ubuntu@ip-172-31-47-110.us-west-2.compute.internal>
2015-08-17 20:49:43 +0000

    adding comments

--- api-test.js
+++ api-test.js
@@ -2,35 +2,52 @@ if (Meteor.isClient) {
 
     var latlon = [];
     var query = '';
+    //load the geojson boundary file
     $.ajax({
 	dataType: 'json',
 	url: '/ecodistrict.geojson',
 	success: function(ecodistrict){
-	    console.log(ecodistrict);
+
+	    //find the boundary box using turf
 	    var bbox = turf.extent(ecodistrict);
-	    console.log(bbox);
+
+	    //Write the query for the API endpoint using the boundary box
 	    query = "?$where=within_box(location_1,"+bbox[3]+","+bbox[0]+","+bbox[1]+","+bbox[2]+")"; 
+
+	    //Get the data from the endpoint using the query
 	    HTTP.get("https://data.kcmo.org/resource/kbzx-7ehe.json" + query, function(err, result){
+
+		//Console log the data so we can look at it
     		console.log(result);
+
+		//Leaflet was getting confused about where the marker image files are located
     		L.Icon.Default.imagePath = '/packages/fuatsengul_leaflet/images';
-		var map = L.map('map',{zoomControl:false}).setView([39.0997, -94.5783], 13);
+
+		//Make the Map
+		var map = L.map('map',{zoomControl:false});
+
+		//Add the basemap layer
     		L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     		    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     		}).addTo(map);
+
+		//Add the geojson boundary file
     		L.geoJson(ecodistrict).addTo(map);
-	  
-	  
-    		result.data.forEach(function(entry, index){
+
+		//Get the bounds of the geojson and fit map to the bounds
+		var bounds = L.geoJson(ecodistrict).getBounds();
+		map.fitBounds(bounds);
+
+
+		//Determine whether each row of data is within the boundary and add it if it is
+	   	result.data.forEach(function(entry, index){
     		    latlon = [entry.location_1.latitude, entry.location_1.longitude]
-		    console.log(L.marker(latlon).toGeoJSON());
 		    var point = L.marker(latlon).toGeoJSON();
-		    console.log(point);
-		    console.log(turf.inside(point, ecodistrict.features[0]));
-		   if(turf.inside(point,ecodistrict.features[0])){
+		    if(turf.inside(point,ecodistrict.features[0])){
 			L.geoJson(point).addTo(map).bindPopup(entry.description+', '+entry.from_date);
 		    }
     		});
-    		return result;
+    		
 	    });
 	}
     });
